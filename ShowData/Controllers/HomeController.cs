8c6efb21604c58209874fbe8eb1f79a2e38e57dd
Muhammad Umair Ather <umair_cvt@outlook.com>
2019-07-22 13:07:56 +0500

    Add project files.

---
+++ ShowData/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using ShowData.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShowData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            Mn_Info_Repository mnInfo = new Mn_Info_Repository();
+            ModelState.Clear();
+
+            ViewBag.info = mnInfo.Get_MnInfo(id);
+            return View();
+        }
+        //public ActionResult SearchAction()
+        //{
+        //    Mn_Info_Repository mnInfo = new Mn_Info_Repository();
+        //    ModelState.Clear();
+
+        //    ViewBag.info = mnInfo.Get_MnInfo(id);
+        //    return View();
+        //}
+
+    }
+}
---
+++ ShowData/Repository/Mn_Info_Repository.cs
@@ -0,0 +1,56 @@
+﻿using ShowData.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Data;
+using System.Web;
+
+namespace ShowData.Repository
+{
+    public class Mn_Info_Repository
+    {
+        private SqlConnection con;
+        //To Handle connection related activities
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DbConnection"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        //To view employee details with generic list 
+        public List<Models.Get_Mn_Info_Result> Get_MnInfo(int? id)
+        {
+            connection();
+            List<Get_Mn_Info_Result> EmpList = new List<Get_Mn_Info_Result>();
+            SqlCommand com = new SqlCommand("Get_Mn_Info", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@tx_id", id);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+
+            //Bind EmpModel generic list using LINQ 
+            EmpList = (from DataRow dr in dt.Rows
+
+                       select new Get_Mn_Info_Result()
+                       {
+                           username = Convert.ToString(dr["username"]),
+                           consumedshares = Convert.ToInt32(dr["consumedshares"]),
+                           remaining = Convert.ToInt32(dr["remaining"]),
+                           packof = Convert.ToInt32(dr["packof"]),
+                           label = Convert.ToString(dr["label"]),
+                           shareadded = Convert.ToInt32(dr["shareadded"])
+
+                       }).ToList();
+
+
+            return EmpList;
+
+
+        }
+    }
+}
